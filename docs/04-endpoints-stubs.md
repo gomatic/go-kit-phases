43cfe17e4a58c6b3fd0361c2aad20174e895676a
nicerobot <nicerobot@users.noreply.github.com>
2020-12-27 22:09:23 -0800

    04-endpoints-stubs

---
+++ docs/04-endpoints-stubs.md
@@ -0,0 +1,1 @@
+This branch [adds](https://github.com/gomatic/go-kit-phases/compare/03-service-basic-tests...04-endpoints-stubs) [stubbed endpoints](../internal/api/endpoint/endpoint.go). 
--- internal/api/endpoint/endpoint.go
+++ internal/api/endpoint/endpoint.go
@@ -1,1 +1,63 @@
 package endpoint
+
+import (
+	"context"
+
+	"github.com/go-kit/kit/endpoint"
+	"github.com/gomatic/go-kit-phases/api/moody"
+)
+
+//
+type Set struct {
+	CreateEndpoint   endpoint.Endpoint
+	RetrieveEndpoint endpoint.Endpoint
+	UpdateEndpoint   endpoint.Endpoint
+	DeleteEndpoint   endpoint.Endpoint
+	ListEndpoint     endpoint.Endpoint
+}
+
+//
+func New(svc moody.SelfServer) Set {
+	return Set{
+		CreateEndpoint:     NewCreateEndpoint(svc),
+		RetrieveEndpoint:   NewRetrieveEndpoint(svc),
+		UpdateEndpoint:     NewUpdateEndpoint(svc),
+		DeleteEndpoint:     NewDeleteEndpoint(svc),
+		ListEndpoint:       NewListEndpoint(svc),
+	}
+}
+
+//
+func NewCreateEndpoint(s moody.SelfServer) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		panic("implement me")
+	}
+}
+
+//
+func NewRetrieveEndpoint(s moody.SelfServer) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		panic("implement me")
+	}
+}
+
+//
+func NewUpdateEndpoint(s moody.SelfServer) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		panic("implement me")
+	}
+}
+
+//
+func NewDeleteEndpoint(s moody.SelfServer) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		panic("implement me")
+	}
+}
+
+//
+func NewListEndpoint(s moody.SelfServer) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		panic("implement me")
+	}
+}
